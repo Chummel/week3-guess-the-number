62872b2951b1c90b6a66caa3479d3bf536dd1b42
Colin Hummel <chummel4@mail.valenciacollege.edu>
2016-01-28 19:34:44 -0500

    again

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -73,15 +73,15 @@ def evaluateAnswer( userGuess, userSecretNumber ):
 #       False, we won't show the right answer on the screen
 def playGame( showAnswer ):
     
-	print:('Welcome to the number guessing game! Please input the upper limit of the number you'd like to guess!')
+	print:('Welcome to the number guessing game! Please input the upper limit of the number you would like to guess!')
 	topLimit = int(input())
 
-	print:(Please choose the number of times you'd like to guess the number.')
+	print:('Please choose the number of times you would like to guess the number.')
 	totalGuesses = int(input())
 
 	theNumber = generateNumber(topLimit)
 
-	print:('Please guess a number between 1 and ' + str(topLimit) + '. You have ' + str(totalGuesses) + ' remaining.')
+	print:('Please guess a number between 1 and ' + str(topLimit) + ' . ' ' You have ' + str(totalGuesses) + ' remaining.')
 
     # TO DO: ####################################################
     # Write code in this function that                          #
